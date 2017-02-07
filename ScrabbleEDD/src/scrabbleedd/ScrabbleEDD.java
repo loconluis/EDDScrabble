89a6438186030d7b33bec4ff47a83fb9a13b6df7
luisl <luisl@192.168.0.5>
2017-02-06 20:00:03 -0600

    Inicio del proyecto

---
+++ ScrabbleEDD/src/scrabbleedd/ScrabbleEDD.java
@@ -0,0 +1,19 @@
+package scrabbleedd;
+
+/**
+ *  FIUSAC
+ *  Estructuras de Datos
+ *  201114279
+ * @author luislocon
+ * @version 1.0
+ */
+public class ScrabbleEDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
